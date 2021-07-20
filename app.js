937f34fd8e04edc834a0901b56e6def8cdcb3c3e
shadow-oss <55443830+shadow-oss@users.noreply.github.com>
2021-07-20 16:25:29 +0500

    commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 function check(){
     var d=document.querySelector("#dt").value;
     var txt=parseInt(document.querySelector("#txt1").value);
+    (console.log(d.length>0)&&(txt>0));
     var sum=0;
-    d=d.replaceAll('-','');
+    if((d.length>0)&&(txt>0)){
+        console.log("Enter");
+        d=d.replaceAll('-','');
     for(var i=0;i<d.length;i++){
         sum=sum+parseInt(d[i]);
     }
@@ -14,4 +17,7 @@ function check(){
         document.querySelector("#check1").style.display="block";
         document.querySelector("#check2").style.display="none";
     }
+
+    }
+    
 }
